516d13241c52473adcbfbee16c3b2106d0017b2b
Liviu Varia <liviu.varia16@gmail.com>
2021-07-07 12:31:36 +0300

    end running container

--- controller.py
+++ controller.py
@@ -1,12 +1,10 @@
 import docker
-from docker.api import container
-import docker
 
 # return a client configured from environment variables.
 client = docker.from_env()
 
 # return a container 
-uas = client.containers.run("ctaloi/sipp","-sn uas", detach=True)
+uas = client.containers.run("ctaloi/sipp", "-sn uas", detach=True)
 print(uas.status)
 
 # list with all containers
@@ -14,9 +12,18 @@ containers_list = client.containers.list()[0]
 
 # if the container is still working, stop it
 while(1):
-    if uas.status == "running" or uas.status == "created":
+    # updateing status
+    uas.reload()
+    if uas.status == "created":
+        print("container just created, wait for changeing status")
         continue
-    else:
+    elif uas.status == "exited":
+        print("container terminated")
         break
+    # container still running
+    else:
+        print("the container will stop")
+        uas.stop()
 
+uas.reload()
 print(uas.status)

7d65f76b0810d830c1b10cc2272f456ed6fba176
Liviu Varia <liviu.varia16@gmail.com>
2021-08-26 12:10:02 +0300

    added network class

---
+++ framework/networks/network.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+##
+## TODO: update project's name
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+
+class Network():
+    def __init__(self, type):
+        self.type = type
+        
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4

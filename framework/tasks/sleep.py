6c2e961243404434031ccd3bbb9e4b2b16ae605c
Razvan Crainea <razvan@opensips.org>
2023-01-12 09:24:22 +0000

    add sleep task

---
+++ framework/tasks/sleep.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+##
+## TODO: update project's name
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+
+"""Sleep runner class"""
+
+import os
+from framework import logger
+from framework.tasks.task import Task
+
+class SleepTask(Task):
+
+    """Sleep class"""
+
+    default_image = "debian"
+
+    def __init__(self, test_dir, config):
+
+        super().__init__(test_dir, config)
+        self.seconds = config.get("seconds", 0)
+        self.milliseconds = config.get("milliseconds", 0) / 1000
+        if self.seconds > self.milliseconds:
+            self.timeout = self.milliseconds
+        else:
+            self.timeout = self.seconds
+
+    def get_task_args(self):
+        return [ "sleep", str(self.timeout) ]
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4

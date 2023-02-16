0954b60276d2190be94c8c2bc71faa9201e33a65
Razvan Crainea <razvan@opensips.org>
2023-02-16 14:06:08 +0000

    Add copyright information

--- sipssert/tracer.py
+++ sipssert/tracer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 ##
 ## This file is part of the SIPssert Testing Framework project
+## Copyright (C) 2023 OpenSIPS Solutions
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by

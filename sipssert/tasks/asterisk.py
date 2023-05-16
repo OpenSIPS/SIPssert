796ca7d4f13d7ce9a4937b04bee894dd44f00c6b
Yaroslav Napadailo <napadailo@kolesa.kz>
2023-05-16 19:16:45 +0600

    Add some config files

--- sipssert/tasks/asterisk.py
+++ sipssert/tasks/asterisk.py
@@ -17,18 +17,38 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
+import os
 from sipssert.task import Task
 
 class AsteriskTask(Task):
 
-    default_mount_point = "/etc/asterisk"
     default_image = "yaroslavonline/asterisk"
+    default_mount_point = "/home"
     default_daemon = True
+
+    asterisk_modules_conf = "/etc/asterisk/modules.conf"
+    asterisk_pjsip_conf = "/etc/asterisk/pjsip.conf"
+    asterisk_extensions_conf = "/etc/asterisk/extensions.conf"
+    asterisk_extensions_lua = "/etc/asterisk/extensions.lua"
+    asterisk_logger_conf = "/etc/asterisk/logger.conf"
+
+    def __init__(self, test_dir, config):
+        super().__init__(test_dir, config)
+
+        self.config = config.get("config")
+        self.modules_file = self.config.get("modules") if isinstance(self.config, dict) else None
+        self.pjsip_file = self.config.get("pjsip") if isinstance(self.config, dict) else None
+        self.logger_file = self.config.get("logger") if isinstance(self.config, dict) else None
+
+        if self.modules_file:
+            self.add_volume_dir(os.path.join(test_dir, self.modules_file),
+                dest=self.asterisk_modules_conf)
+
+        if self.pjsip_file:
+            self.add_volume_dir(os.path.join(test_dir, self.pjsip_file),
+                dest=self.asterisk_pjsip_conf)
     
-    def get_task_args(self):
-
-        args = []
-
-        return args
-
+        if self.logger_file:
+            self.add_volume_dir(os.path.join(test_dir, self.logger_file),
+                dest=self.asterisk_logger_conf)
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4

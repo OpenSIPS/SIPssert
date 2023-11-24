825fa279390c9ab19ded98af013fac339cd7e3cb
Razvan Crainea <razvan@opensips.org>
2023-11-24 12:16:19 +0200

    github: use environment.yml instead of matrix
    The environment variable should contain more information about the
    running envirnment, not just the matrix; thus, we need to create the
    matrix with only the information we need.

--- actions/read_matrix.py
+++ actions/read_matrix.py
@@ -1,3 +1,11 @@
-import sys, json, yaml
+"""Filters the OS and tests_set from the matrix file"""
+import sys
+import json
+import yaml
 
-print(json.dumps(yaml.safe_load(open(sys.argv[1]))))
+matrix_keys = [ "os", "test_set" ]
+
+matrix_dict = yaml.safe_load(open(sys.argv[1], encoding="utf-8"))
+
+print(json.dumps(dict(filter(lambda p: p[0] in matrix_keys,
+                             matrix_dict.items()))))

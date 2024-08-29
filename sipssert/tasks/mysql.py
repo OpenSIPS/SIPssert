6afe287075651dff1628464232ac969ddca88616
Razvan Crainea <razvan@opensips.org>
2024-08-29 21:27:09 +0300

    simplify mysql and postgres env handling

--- sipssert/tasks/mysql.py
+++ sipssert/tasks/mysql.py
@@ -27,15 +27,9 @@ class MysqlTask(Task):
     default_mount_point = "/docker-entrypoint-initdb.d"
 
     def get_task_env(self):
-        if "env_file" in self.config:
-            env_file_dict = self.parse_env_file(self.config["env_file"])
-        else:
-            env_file_dict = {}
-
-        self.mysql_default_env.update(env_file_dict)
-        self.mysql_default_env.update(self.config.get("env", {}))
 
         env_dict = self.mysql_default_env
+        env_dict.update(super().get_task_env())
 
         if "root_password" in self.config:
             self.root_password = self.config["root_password"]
